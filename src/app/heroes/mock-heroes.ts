c9814ad93020dc5462f23b094a118b73262de769
Zsuzsi Hudacsko <zsuzsi.hudacsko@gmail.com>
2018-08-09 10:31:40 +0200

    4th point is done

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  { id: 1, name: '12th Doctor' },
+  { id: 2, name: 'Jack Harkness' },
+  { id: 3, name: 'Amy' },
+  { id: 4, name: '11th Doctor' },
+  { id: 5, name: 'River Song' }
+]
